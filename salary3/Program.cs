529e51351d93b016bb4c87499f24fceb6821ad89
Ksenia <besedinaksenya@mail.ru>
2022-08-25 22:52:58 +0400

    Add class Settings for working with configuration files

--- salary3/Program.cs
+++ salary3/Program.cs
@@ -1,25 +1,43 @@
 ﻿using Transfer;
 using WorkWithData;
+using System.Configuration;
+using salary3;
 
 class Master
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        Console.Write("Enter input file path:\t");
-        string inputFile = Console.ReadLine();
-        Console.Write("Enter output file path:\t");
-        string outputFile = Console.ReadLine();
         
+        Settings settings = new Settings();
 
+        //foreach (var pair in settings)
+        //{
+        //    Console.WriteLine(pair);
+        //}
+
+        if (args.Length > 0)
+        { 
+            string customConfig = args[0];
+            settings.AddConfigFile(customConfig);
+        }
+
+        string inputFile = settings.GetSetting("SourceFile");
+        string outputFile = settings.GetSetting("DestFile");
+        string bonus = settings.GetSetting("BonusPercent");       
+        
         var database = new List<User>();
         FileHandler.Read(inputFile, database);
 
-        Console.Write("Enter premium rate in percent format:\t");
-        int premium = int.Parse(Console.ReadLine());
+        //Console.Write("Enter premium rate in percent format:\t");
+        //int premium = int.Parse(Console.ReadLine());
+
+        int premium = int.Parse(bonus);
+        
 
         foreach (var i in database)
             i.Salary *= ((double)premium / 100 + 1);
 
         FileHandler.Write(outputFile, database);
     }
+
 }
---
+++ salary3/Settings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary3
+{
+    public class Settings : ICloneable, 
+        IEnumerable<KeyValuePair<string, string>> 
+          
+    {
+        
+        private readonly IDictionary<string, string> dict = new Dictionary<string, string>();   
+
+        private Settings(Settings s)
+        {
+            dict = new Dictionary<string, string>(s.dict);
+        }
+        
+        public Settings() 
+        {
+            AddConfigFile("Global.cfg");
+
+            string userConfigFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\User.cfg";
+            if (File.Exists(userConfigFile)) 
+            {
+                AddConfigFile(userConfigFile);
+            }
+        }
+
+        public string GetSetting(string key) 
+        { 
+            return dict[key];
+        }
+        public void AddConfigFile(string path)
+        {        
+            using (StreamReader file = new StreamReader(path))
+            {
+                while (!file.EndOfStream)
+                {
+                    var fileLine = file.ReadLine();
+                    var fragments = fileLine.Split('=', 2);
+                    string key = fragments[0];
+                    string value = fragments[1];
+                    dict[key] = value;
+                }
+            }
+        }
+
+        public object Clone()
+        {
+            return new Settings(this);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return dict.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
+    }
+}
